f6822b9b1b3e8f2a09645531365f7e215cec3f39
evertholivas12@gmail.com <elirafa2511@gmail.com>
2019-11-09 09:57:00 -0600

    segundo

--- src/boot/setup.js
+++ src/boot/setup.js
@@ -1,8 +1,6 @@
 import * as Expo from "expo";
 import { Font } from 'expo';
-import { Ionicons } from '@expo/vector-icons';
-import React, { Component } from "react";
-import { StyleProvider } from "native-base";
+
 
 import App from "../App";
 import getTheme from "../theme/components";
@@ -20,20 +18,9 @@ export default class Setup extends Component {
   }
   async loadFonts() {
     await Font.loadAsync({
-      Roboto: require("native-base/Fonts/Roboto.ttf"),
-      Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf"),
-      Ionicons: require("@expo/vector-icons/fonts/Ionicons.ttf"),
-      Entypo: require("native-base/Fonts/Entypo.ttf"),
-      Feather: require("native-base/Fonts/Feather.ttf"),
-      FontAwesome: require("native-base/Fonts/FontAwesome.ttf"),
-      // MaterialIcons: require("native-base/Fonts/MaterialIcons.ttf"),
-      // MaterialCommunityIcons: require("native-base/Fonts/MaterialCommunityIcons.ttf"),
-      Octicons: require("native-base/Fonts/Octicons.ttf"),
-      // Zocial: require("@expo/vector-icons/fonts/Zocial.šttf"),
-      // SimpleLineIcons: require("native-base/Fonts/SimpleLineIcons.ttf"),
-      // EvilIcons: require("native-base/Fonts/EvilIcons.ttf"),
-      // ...Ionicons.font,
-    });
+      Roboto: require("../../node_modules/native-base/Fonts/Roboto.ttf"),//
+      Roboto_medium: require("../../node_modules/native-base/Fonts/Roboto_medium.ttf"),//
+     });
     this.setState({ isReady: true });
   }
   render() {
