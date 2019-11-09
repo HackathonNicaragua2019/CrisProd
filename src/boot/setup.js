66c588c37f2c02443ba5356d815855ec191a6746
evertholivas12@gmail.com <elirafa2511@gmail.com>
2019-11-09 14:36:36 -0600

    version 2.0

--- src/boot/setup.js
+++ src/boot/setup.js
@@ -1,8 +1,10 @@
 import * as Expo from "expo";
-import { Font } from 'expo';
+import * as Font  from 'expo-font';
+//import { Ionicons } from '@expo/vector-icons';
+import React, { Component } from "react";
+import { StyleProvider } from "native-base";
 
-
-import App from "../App";
+import App from "../app";
 import getTheme from "../theme/components";
 import variables from "../theme/variables/commonColor";
 
@@ -20,7 +22,7 @@ export default class Setup extends Component {
     await Font.loadAsync({
       Roboto: require("../../node_modules/native-base/Fonts/Roboto.ttf"),//
       Roboto_medium: require("../../node_modules/native-base/Fonts/Roboto_medium.ttf"),//
-     });
+    });
     this.setState({ isReady: true });
   }
   render() {
